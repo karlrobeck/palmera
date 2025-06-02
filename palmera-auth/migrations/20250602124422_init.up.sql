97465a55cb43a6c4bacbad5dd6389691b6bbd44a
Karl robeck alferez <karlalferezfx@gmail.com>
2025-06-03 03:02:44 +0800

    Add auth-test package and update workspace members; enforce unique email constraint in auth_users table

--- palmera-auth/migrations/20250602124422_init.up.sql
+++ palmera-auth/migrations/20250602124422_init.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 create table auth_users (
   id uuid_text not null primary key,
-  email email_text not null,
+  email email_text not null unique,
   password password_text not null,
   created datetime_text not null,
   updated datetime_text not null

46f364edb6e805cd49029d06059208263d378ff6
Karl robeck alferez <karlalferezfx@gmail.com>
2025-06-08 23:42:34 +0800

    Add UUID dependency and enhance hook functionality with improved handler management

--- palmera-core/src/hook.rs
+++ palmera-core/src/hook.rs
@@ -1,7 +1,9 @@
+use uuid::Uuid;
+
 pub struct Handler<T> {
     func: Box<dyn Fn(&mut T) -> Result<(), anyhow::Error>>,
-    id: String,
-    priority: i16,
+    id: Option<String>,
+    priority: Option<i16>,
 }
 
 pub struct Hook<T> {
@@ -13,52 +15,69 @@ impl<T> Hook<T> {
         Self { handlers: vec![] }
     }
 
-    pub fn bind<F>(&mut self, callback: F)
+    pub fn bind(&mut self, handler: Handler<T>) -> String {
+        let mut handler = handler;
+
+        if handler.id.is_none() {
+            handler.id = Some(generate_hook_id());
+
+            for existing in &self.handlers {
+                if existing.id == handler.id {
+                    handler.id = Some(generate_hook_id());
+                }
+            }
+        }
+
+        let id = handler.id.clone().unwrap();
+
+        self.handlers.push(handler);
+
+        self.handlers.sort_by_key(|h| h.priority.unwrap_or(0));
+
+        return id;
+    }
+
+    pub fn bind_fn<F>(&mut self, callback: F) -> String
     where
         F: Fn(&mut T) -> Result<(), anyhow::Error> + Send + 'static,
     {
-        self.handlers.push(Handler {
+        self.bind(Handler {
             func: Box::new(callback),
-            id: "".into(),
-            priority: -1,
-        });
+            id: None,
+            priority: None,
+        })
+    }
+
+    pub fn unbind(&mut self, id: String) -> anyhow::Result<()> {
+        let original_len = self.handlers.len();
+
+        self.handlers
+            .retain(|handler| handler.id.as_deref() != Some(&id));
+
+        if self.handlers.len() == original_len {
+            Err(anyhow::anyhow!("Handler with id {} not found", id))
+        } else {
+            Ok(())
+        }
     }
 
     pub fn length(&self) -> usize {
         self.handlers.len()
     }
 
-    pub fn trigger(&mut self, value: &mut T) {
+    pub fn trigger(&mut self, value: &mut T) -> Vec<anyhow::Error> {
+        let mut errors = vec![];
+
         for handler in &self.handlers {
             if let Err(err) = (handler.func)(value) {
-                println!("{err}");
+                errors.push(err);
             }
         }
+
+        errors
     }
 }
 
-#[cfg(test)]
-mod test {
-    use crate::hook::Hook;
-
-    struct FooStruct {
-        bar: String,
-    }
-
-    #[test]
-    fn test() {
-        let mut hook: Hook<FooStruct> = Hook::new();
-
-        hook.bind(|v: &mut FooStruct| {
-            v.bar = "hello".into();
-            Ok(())
-        });
-
-        let mut foo = FooStruct {
-            bar: "hello world".into(),
-        };
-        _ = hook.trigger(&mut foo);
-
-        assert_eq!(foo.bar, "hello");
-    }
+fn generate_hook_id() -> String {
+    Uuid::new_v4().to_string()
 }
